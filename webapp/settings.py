6937379a436c002fdf60d7ac0c01f7437305b32b
Ananda Mohon Ghosh <gdm.anandamohon@gmail.com>
2021-06-27 21:34:10 -0400

    code bug fixed

--- webapp/settings.py
+++ webapp/settings.py
@@ -76,11 +76,24 @@ WSGI_APPLICATION = 'webapp.wsgi.application'
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'realstate_db',
+        'USER': 'postgres',
+        'PASSWORD': '7824',
+        'HOST':'localhost'
+    }
+}
+
+'''
+#SQL Lite db connection 
+DATABASES = {
+    'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
+'''
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
